08c1210f6643af33b9d232d8c19388ba8b18bf22
Victor De Lomas <victordlp8@gmail.com>
2024-10-15 15:48:38 +0200

    Cleaning

--- ore-jni-lib/src/utils.rs
+++ ore-jni-lib/src/utils.rs
@@ -1,10 +1,5 @@
 use jni::JNIEnv;
 use jni::objects::JString;
-use jni::sys::{jint, jboolean};
-
-use either::Either;
-
-type JNIResult = Result<Either<String, Either<u64, bool>>, String>;
 
 pub fn string_unwrap(env: &mut JNIEnv, data: JString) -> String {
     env.get_string(&data)
